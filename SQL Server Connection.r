6719241c2d66800926a78a41237a2ae461d87834
jkuziemko <jeff.kuziemko@gmail.com>
2018-02-23 15:43:05 -0500

    Added comment

--- SQL Server Connection.r
+++ SQL Server Connection.r
@@ -1,3 +1,4 @@
+#Connect to SQL Server
 library(RODBC)
 ch <- odbcDriverConnect('driver={SQL Server};server=10.88.1.228;db=Props;trusted_connection=true')
 sqlQuery(ch, 'Select count(*) FROM [Props].[dbo].[AllHotelDetails]')
